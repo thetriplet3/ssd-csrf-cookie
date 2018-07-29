c0dc6172f8a2ad55c5f61d11d2177d0eaf8d60fe
thetriplet3 <thetriplet3@gmail.com>
2018-07-30 01:57:07 +0530

    Add project files.

---
+++ csrf-cookie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace csrf_cookie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
